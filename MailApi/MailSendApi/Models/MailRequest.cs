a4e2dcffb1f8f8de5777b13d9fc7aca305e40cbf
SahuGit <56160645+SahuGit@users.noreply.github.com>
2023-01-25 18:42:36 +0530

    MailService
    MailService

---
+++ MailApi/MailSendApi/Models/MailRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailSendApi.Models
+{
+    public class MailRequest
+    {
+        public string ToEmail { get; set; }
+        public String Subject { get; set; }
+        public String Body { get; set; }
+        public string From { get; set; }
+    }
+}
---
+++ MailApi/MailSendApi/Services/MailService.cs
@@ -0,0 +1,45 @@
+﻿using MailSendApi.Models;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace MailSendApi.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+    }
+    public class MailService : IMailService
+    {
+        private readonly MailSettings _mailSettings;
+        public MailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(MailRequest mailRequest)
+        {
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(mailRequest.From);
+
+            mail.To.Add(mailRequest.ToEmail);
+            mail.Subject = mailRequest.Subject;
+            mail.Body = mailRequest.Body;
+            mail.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient(_mailSettings.Host, _mailSettings.Port);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential(_mailSettings.Mail, _mailSettings.Password);
+            smtp.EnableSsl = true;
+            Thread T1 = new Thread(delegate ()
+            {
+                smtp.Send(mail);
+            });
+
+            T1.Start();
+        }
+    }
+}
